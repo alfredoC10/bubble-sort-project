743a3ba37efdebd3541001eb7e872a436243c5d7
Huemac-Alfredo <hmca101292@outlook.com>
2020-10-27 16:53:11 -0600

    Fix linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,18 +2,14 @@
 def bubble_sort(the_array)
   count = the_array.length - 1
 
-  if the_array.all? Numeric
-    count.times do 
-      for i in 0..count
+  return "Incorrect array" unless the_array.all? Numeric
+    count.times do
+      (0..count).each do |i|
         if (the_array[i].to_f > the_array[i + 1].to_f) && (i != count)
           the_array[i], the_array[i + 1] = the_array[i + 1], the_array[i]
         end
-      end 
+      end
     end
-
-  else
-    return "Incorrect array"
-  end
 
   print the_array
 end
@@ -31,17 +27,17 @@ def bubble_sort_by(str_arr)
 
   else
     counting.times do 
-      for j in 0..iterate
-        next unless yield(str_arr[j], str_arr[j + 1]) > 0
-          str_arr[j], str_arr[j + 1] = str_arr[j + 1], str_arr[j]
+      (0..iterate).each do |j|
+        next unless yield(str_arr[j], str_arr[j + 1]).positive?
+
+        str_arr[j], str_arr[j + 1] = str_arr[j + 1], str_arr[j]
       end 
     end
   end
   print str_arr
-end 
-
-a_case = ["regards!", "regards", "hello", "hey", "salute", "hi"]
-bubble_sort_by(a_case) do |left, right| 
-  left.length - right.length
 end
 
+a_case = ['regards!', 'regards', 'hello', 'hey', 'salute', 'hi']
+bubble_sort_by(a_case) do |left, right|
+  left.length - right.length
+end
