31462109ba30d5f68c77101a16a78f490a338eb7
Huemac-Alfredo <hmca101292@outlook.com>
2020-10-27 17:23:25 -0600

    Fix few more linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,14 +2,15 @@
 def bubble_sort(the_array)
   count = the_array.length - 1
 
-  return "Incorrect array" unless the_array.all? Numeric
-    count.times do
-      (0..count).each do |i|
-        if (the_array[i].to_f > the_array[i + 1].to_f) && (i != count)
-          the_array[i], the_array[i + 1] = the_array[i + 1], the_array[i]
-        end
+  return 'Incorrect array' unless the_array.all? Numeric
+  
+  count.times do
+    (0..count).each do |i|
+      if (the_array[i].to_f > the_array[i + 1].to_f) && (i != count)
+        the_array[i], the_array[i + 1] = the_array[i + 1], the_array[i]
       end
     end
+  end
 
   print the_array
 end
@@ -23,7 +24,7 @@ def bubble_sort_by(str_arr)
   iterate = str_arr.length - 2
 
   if str_arr.any? Numeric
-    print "Wrong array"
+    print 'Wrong array'
 
   else
     counting.times do 
@@ -31,7 +32,7 @@ def bubble_sort_by(str_arr)
         next unless yield(str_arr[j], str_arr[j + 1]).positive?
 
         str_arr[j], str_arr[j + 1] = str_arr[j + 1], str_arr[j]
-      end 
+      end
     end
   end
   print str_arr
